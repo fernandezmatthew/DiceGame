f95d14b18bf3ac82929b6d4c165ef3a0d5ef0ee7
fernandezmatthew <fernandezmatthewkyle@gmail.com>
2026-01-24 12:25:41 -0500

    Change Cheeseburger Logic
    Add new option to main menu

--- Assets/ScriptableObjects/Chef/Dishes/Cheeseburger.cs
+++ Assets/ScriptableObjects/Chef/Dishes/Cheeseburger.cs
@@ -13,24 +13,16 @@ public class CheeseburgerDish : Dish {
         int score = baseValue;
 
         //Check dice for conditional values
-        int maxBeefGrade = 1;
-        int cheeseCount = 0;
+        int totalBeefGrade = 0;
         foreach (var die in dice) {
             if (die.CurrentFace.ingredient == Ingredient.EIngredientType.Beef) {
-                if (die.CurrentFace.grade.value > maxBeefGrade) {
-                    maxBeefGrade = die.CurrentFace.grade.value;
-                }
-            }
-            else if (die.CurrentFace.ingredient == Ingredient.EIngredientType.Cheese) {
-                cheeseCount++;
+                totalBeefGrade += die.CurrentFace.grade.value;
             }
         }
 
         //Adjust score based on conditions
-        score += maxBeefGrade;
-        if (cheeseCount > 1) {
-            score += 4;
-        }
+        score += totalBeefGrade;
+        
         return score;
     }
 }
