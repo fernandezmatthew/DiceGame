72717ff85272459f425000c3a6d51b1da97090ec
fernandezmatthew <fernandezmatthewkyle@gmail.com>
2026-01-24 02:01:51 -0500

    Refector DishDisplay
    Made public ui variables private since they can be found in code consistently and
    reliably.

--- Assets/Display/Chef/DisplayScripts/DishDisplay.cs
+++ Assets/Display/Chef/DisplayScripts/DishDisplay.cs
@@ -7,19 +7,31 @@ using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
 public class DishDisplay : MonoBehaviour, IPointerClickHandler {
-    public UnityEvent<Dish> dishClicked;
+    [HideInInspector] public UnityEvent<Dish> dishClicked;
 
-    public TMP_Text dishName;
-    public Image dishImage;
+    private TMP_Text dishName;
+    private Image dishImage;
 
     public Dish dish;
 
     private void OnValidate() {
+        if (dishName == null) {
+            dishName = GetComponent<TMP_Text>();
+        }
+        if (dishImage == null) {
+            dishImage = GetComponent<Image>();
+        }
         if (dishName != null && dishImage != null && dish != null) {
             UpdateDisplay();
         }
     }
     public void Start() {
+        if (dishName == null) {
+            dishName = GetComponent<TMP_Text>();
+        }
+        if (dishImage == null) {
+            dishImage = GetComponent<Image>();
+        }
         UpdateDisplay();
     }
     public void SetDish(Dish dish) {
