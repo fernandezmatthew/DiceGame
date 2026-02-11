013697b5c3362051f058afcffc7f6af19de4cfe7
fernandezmatthew <fernandezmatthewkyle@gmail.com>
2026-02-11 13:12:30 -0500

    Add more save page stuff

---
+++ Assets/Scripts/ScrollRectNoDrag.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ScollRectNoDrag : ScrollRect
+{
+    public override void OnBeginDrag(PointerEventData eventData) { }
+
+    public override void OnDrag(PointerEventData eventData) { }
+
+    public override void OnEndDrag(PointerEventData eventData) { }
+}
